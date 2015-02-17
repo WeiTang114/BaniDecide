f9428c217b2e7acfee688f659ec89057a2155a18
WeiTang114 <weitang114@gmail.com>
2015-02-17 17:03:30 +0800

    Fix bug, Finish selectItem

--- BaniDecide/lib/parse/CloudCode/cloud/main.js
+++ BaniDecide/lib/parse/CloudCode/cloud/main.js
@@ -40,6 +40,7 @@ Parse.Cloud.define("getQuestion", function(request, response) {
     }
     response.success(JSON.stringify(qResp));
   }, function(error) {
+    console.log("Error A: " + error)
     response.error(JSON.stringify({"failtype":error}));
   });
 });
@@ -54,7 +55,7 @@ Parse.Cloud.define("getQuestion", function(request, response) {
  */
 Parse.Cloud.define("addQuestion", function(request, response) {
   var items = request.params.items;
-  if (typeof items === 'undefined' && items == null) {
+  if (typeof items === 'undefined' || items == null) {
     response.error(
       JSON.stringify({failtype: RESP_BADREQUEST + 'correct: {"qid":qid}'})
     );
@@ -69,6 +70,7 @@ Parse.Cloud.define("addQuestion", function(request, response) {
     var qid = qAgain.id;
     response.success(JSON.stringify({"qid":qid}));
   }, function(error) {
+    console.log("Error B: " + error)
     response.error(JSON.stringify({"failtype":error}));
   });
 });
@@ -83,5 +85,71 @@ Parse.Cloud.define("addQuestion", function(request, response) {
  *   fail: {"failtype":"message"}
  */
 Parse.Cloud.define("selectItem", function(request, response) {
-  
+  var uid = request.params.uid;
+  var qid = request.params.qid;
+  var number = request.params.number;
+
+  // check request
+  if (isNullOrUndef(uid) || 
+      isNullOrUndef(qid) ||
+      isNullOrUndef(number)) {
+    response.error(
+      JSON.stringify({failtype: RESP_BADREQUEST + 'correct: {"uid":uid, "qid":qid, "number":n}'})
+    );
+  }
+
+  var MyUser = Parse.Object.extend("MyUser");
+  var query = new Parse.Query(MyUser);
+  var myUser;
+  query.equalTo("uid", uid);
+  query.find().then(function(myUserList) {
+    console.log("myUserList:" + JSON.stringify(myUserList));
+
+    // user not exist yet
+    if (myUserList.length == 0) {
+      var newUser = new MyUser();
+      newUser.set("uid", uid);
+      newUser.set("answeredQids", []);
+      return newUser.save(null);
+    }
+
+    myUser = myUserList[0];
+
+    // check if he/she has voted!
+    if (myUser.toJSON().answeredQids.indexOf(qid) >= 0) {
+      return Parse.Promise.error("You have voted!");
+    }
+  }).then(function(newUserAgain){
+    // new user added
+    myUser = newUserAgain;
+
+    // select!
+    var Question = Parse.Object.extend("Question");
+    query = new Parse.Query(Question);
+    return query.get(qid);
+  }).then(function(question) {
+    // get question object
+    var qJSON = question.toJSON();
+    console.log("item number ++");
+    qJSON.numbers[number] ++;
+    return question.save({numbers: qJSON.numbers});
+  }).then(function(qAgain) {
+    // question item seleted
+    var myUJSON = myUser.toJSON();
+    myUJSON.answeredQids.push(qid);
+    return myUser.save({answeredQids: myUJSON.answeredQids});
+  }).then(function(results){
+    // done
+    response.success("OK");
+  }, function(error) {
+    console.log("Error: " + error)
+    response.error(JSON.stringify({"failtype":error}));
+  });
 });
+
+
+
+function isNullOrUndef(obj) {
+  return (typeof obj === 'undefined' || obj == null);
+}
+

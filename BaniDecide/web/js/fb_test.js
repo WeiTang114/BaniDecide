9b43b7904125a6581207ccb710ae08b6778cb3aa
Kellyfu <b01902030@ntu.edu.tw>
2015-02-27 00:55:17 +0800

    fb_function

---
+++ BaniDecide/web/js/fb_test.js
@@ -0,0 +1,61 @@
+
+var uid;
+var accessToken;
+FB.init({
+	     appId: '303348743168816',
+	     status: true,
+	     cookie: true,
+	     xfbml: true,
+	     version : 'v2.2'
+
+	});
+function getLoginState(){
+
+	FB.getLoginStatus(function (response) {
+		if (response.status === 'connected'){      
+			uid = response.authResponse.userID;
+			return 1;
+		} 
+		else if(response.status === 'not_authorized')
+			return 2;
+		else 
+			return 3;
+	})
+
+}
+function fb_login(){
+
+	FB.login(function (response) {
+			
+		if (response.authResponse) {
+			uid = response.authResponse.userID;
+			accessToken = response.authResponse.accessToken;
+			return true;
+		}
+	}, {
+	scope: 'email,publish_stream'
+	});
+}
+function fb_share()
+{
+
+	FB.ui(
+	  {
+	    method: 'feed',
+	    name: 'DIDADIDI',
+	    //redirect_uri:'http://www.csie.ntu.edu.tw'
+		link: 'http://www.csie.ntu.edu.tw/~b01902030/fb_test/index.html/',
+		picture: 'http://www.csie.ntu.edu.tw/~b01902030/fb_test/pig.jpg',
+		caption: '測試',
+		description: 'testtesttest'
+	  },
+		function(response) {
+		    if (response && response.post_id) {
+		    //	alert('Post was published.');
+			} else {
+			//alert('Post was not published.');
+			}
+	    }
+	);
+
+}

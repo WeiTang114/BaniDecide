2ad7b3f4e3ebb732923d29fb5332778a8deac2e4
Urchin <anabelxrita@gmail.com>
2015-03-26 11:57:36 +0800

    rewrite dart2js function

--- BaniDecide/lib/client/util.dart
+++ BaniDecide/lib/client/util.dart
@@ -3,4 +3,4 @@ library client.util;
 import "dart:html";
 
 String getQuestionId(Location location) 
-  => location.search.isEmpty ? null : location.search.substring(1);
+  => location.search.isEmpty ? null : location.search.substring(4);

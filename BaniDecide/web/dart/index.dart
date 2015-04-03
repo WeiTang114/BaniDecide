20c14ada3b359532260f8c33a6a61f00e50e3a8c
Urchin <anabelxrita@gmail.com>
2015-04-03 18:08:58 +0800

    finish homepage

--- BaniDecide/web/dart/index.dart
+++ BaniDecide/web/dart/index.dart
@@ -1,11 +1,22 @@
 library client.question;
 
-//import "dart:html";
-
-//import 'package:BaniDecide/client/util.dart';
+import 'dart:html';
+import 'package:BaniDecide/client/util.dart';
 import 'package:BaniDecide/client/component.dart';
 
+OtherQuestions obj;
+
 void main() {
-  OtherQuestions obj = new OtherQuestions();
-  obj.generate();
+  initFB().then((_) => getLoginState())
+  .then((response) {
+    String resp = response.toString();
+    
+    if (resp == '1') {
+      querySelector('#create').classes.remove('hidden');
+      obj = new OtherQuestions();
+      obj.generate();
+    } else {
+      querySelector('#fblogin').classes.remove('hidden');
+    }
+  }).catchError((ex) => print(ex));
 }
--- BaniDecide/web/js/fb_utils.js
+++ BaniDecide/web/js/fb_utils.js
@@ -2,7 +2,32 @@ var uid;
 var accessToken;
 var KEY_TOKEN = "access_token";
 
+function initFB(onSuccess) {
+  window.fbAsyncInit = function() {
+    initParse();
+    Parse.FacebookUtils.init({ // this line replaces FB.init({
+      appId      : '303348743168816', // Facebook App ID
+      status     : true,  // check Facebook Login status
+      cookie     : true,  // enable cookies to allow Parse to access the session
+      xfbml      : true,  // initialize Facebook social plugins on the page
+      version    : 'v1.0' // point to the latest Facebook Graph API version
+    });
+ 
+    onSuccess(1);
+  };
+ 
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "http://connect.facebook.net/zh_TW/all.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+}
+
 function getLoginState(onSuccess, onFail) {
+  initParse();
+  
   FB.getLoginStatus(function (response) {
     console.log('getLoginState: status:' + response.status);
     if (response.status === 'connected') {
@@ -80,15 +105,13 @@ function fb_share() {
     picture: 'http://www.csie.ntu.edu.tw/~b01902030/fb_test/pig.jpg',
     caption: '測試',
     description: 'testtesttest'
-    },
-    function(response) {
-      if (response && response.post_id) {
-        //  alert('Post was published.');
-      } else {
-      //alert('Post was not published.');
-      }
+  }, function(response) {
+    if (response && response.post_id) {
+     //alert('Post was published.');
+    } else {
+     //alert('Post was not published.');
     }
-  );
+  });
 }
 
 function saveAccessToken(token) {
